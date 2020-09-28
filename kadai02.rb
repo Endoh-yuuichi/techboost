a8d038839a3ff3453fbfd2fd92c39345d19ede29
”techboostgit config --global user.email ”adsa153@icloud.com <adsa153@icloud.com>
2020-09-28 19:10:47 +0000

    課題１１

---
+++ kadai02.rb
@@ -0,0 +1,16 @@
+num_arry = [1,2,3,4]
+p num_arry[0]
+p num_arry[1]
+p num_arry[2]
+p num_arry[3]
+
+profile = {name: '遠藤',birthday: "1976/03/03",blood_type: "Ｂ型",}
+
+p profile[:name]
+p profile[:birthday]
+p profile[:blood_type]
+
+p profile_ruby = {name: "遠藤",birthday: "1976/03/03",blood_type: "Ｂ型",}
+p profile_ruby[:name]
+p profile_ruby[:birthday]
+p profile_ruby[:blood_type]
